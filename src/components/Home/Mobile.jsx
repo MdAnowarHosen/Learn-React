72894c6dd25d08c86169feb9de4890f0af3f9d2b
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2022-03-28 11:19:51 +0600

    form validation

--- src/components/Home/Mobile.jsx
+++ src/components/Home/Mobile.jsx
@@ -5,12 +5,22 @@ export class Fruits extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
+      mobile_name: "",
       ram: "",
       rom: "",
-      camera: ""
+      camera: "",
+      mobileError: ""
     };
   }
+  // validate data
+  validate() {
+    if (!this.state.mobile_name > 0) {
+      this.setState({ mobileError: "Mobile name required" });
+    } else {
+      return true;
+    }
+  }
+
   ChangeMobile = (event) => {
     let value = event.target.name;
     let getting_value = event.target.value;
@@ -19,19 +29,27 @@ export class Fruits extends PureComponent {
 
   SubmitForm = (e) => {
     e.preventDefault();
-    console.log(this.state.username);
+    if (this.validate()) {
+      alert("validation success");
+    }
   };
   render() {
     return (
       <div>
-        <div>Mobile Name: {this.state.username}</div>
+        <div>Mobile Name: {this.state.mobile_name}</div>
         <div>Mobile Ram: {this.state.ram}</div>
         <div>Mobile Rom: {this.state.rom}</div>
         <div>Mobile Camera: {this.state.camera}</div>
         <div>
           <form onSubmit={this.SubmitForm}>
             <label htmlFor="">Mobile Name</label>
-            <input name="username" type="text" onChange={this.ChangeMobile} />
+            <input
+              name="mobile_name"
+              type="text"
+              onChange={this.ChangeMobile}
+            />
+            <br />
+            <span style={{ color: "red" }}>{this.state.mobileError}</span>
             <br />
             <label htmlFor="">Ram</label>
             <input name="ram" type="text" onChange={this.ChangeMobile} />
